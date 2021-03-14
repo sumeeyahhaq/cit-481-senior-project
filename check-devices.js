325b929ad4c833df7bdf0c16c4e83bed46de72fa
Samvel <svksam@yahoo.com>
2021-03-14 00:08:57 -0800

    Adds opacity to the body when menu is open in mobile view

--- check-devices.js
+++ check-devices.js
@@ -11,7 +11,7 @@ function checkDevices() {
 		!window.MSStream;
 
 	if (isMobile || isIOS) {
-		//Removes class drop on mobile
+		//Removes class drop on mobile because hover does not support on mobiles
 		var classRemove = document.querySelectorAll(".drop");
 		for (var i = 0; i < classRemove.length; i++) {
 			classRemove[i].classList.remove("drop");
@@ -22,38 +22,40 @@ function checkDevices() {
 			$(".navbar-collapse").collapse("hide");
 		});
 
-		//Checks if hamburger button is clicked and menu is visible
+		//Adding opacity to the whole page when menu is open
+
+		// //Checks if hamburger button is clicked and menu is visible
 		$(".navbar").on("click", ".navbar-toggler", function () {
-			var menuIsVisible = $("#navbarTogglerDemo02").is(":visible");
+			var menuIsVisible = $("#navbarNav").is(":visible");
 			checkMenuVisiblity(menuIsVisible);
 		});
 
 		//Checks if hamburger button is clicked and Submenu is visible
 		$(".navbar").on("click", ".dropdown-item", function () {
-			var menuIsVisible = $("#navbarTogglerDemo02").is(":visible");
+			var menuIsVisible = $("#navbarNav").is(":visible");
 			checkMenuVisiblity(menuIsVisible);
 		});
 
-		//This function adds and removes fixedPosition class
+		//This function adds and removes fixedPosition class which gives opacity to the main
 		function checkMenuVisiblity(menuIsVisible) {
 			if (!menuIsVisible) {
-				$("body").addClass("fixedPosition");
+				$("main").addClass("fixedPosition");
 			} else {
-				$("body").removeClass("fixedPosition");
+				$("main").removeClass("fixedPosition");
 			}
 		}
 	}
 
 	// removes drop class on desktop mode to deactivates hover feature
 	else {
-	var dropClass = document.getElementById("remove-hover");
-	$(".navbar").on("click", ".dropdown-item", function () {
-		// removes drop class to disable hover
-		dropClass.classList.remove("drop");
-		//Waits 1 seconds and activates hover feature again
-		setTimeout(function () {
-			dropClass.classList.add("drop");
-		}, 100);
-	});
-	 }
+		var dropClass = document.getElementById("remove-hover");
+		$(".navbar").on("click", ".dropdown-item", function () {
+			// removes drop class to disable hover
+			dropClass.classList.remove("drop");
+			//Waits 1 seconds and activates hover feature again
+			setTimeout(function () {
+				dropClass.classList.add("drop");
+			}, 100);
+		});
+	}
 }
